06e014a3712aff8f193a854f3cfafb38bc911cb4
EnderSash <51605446+EnderSash@users.noreply.github.com>
2019-06-20 23:03:28 +0300

    first commit

---
+++ EnderSash's Mystery/Assets/TLCindie/SciFi Ghost Ship/Auto Door.js
@@ -0,0 +1,18 @@
+﻿var Player : GameObject;
+
+
+function OnTriggerEnter (other : Collider) {
+
+if (other.gameObject == Player)
+
+
+animation.CrossFade("AutoDoor");
+}
+
+function OnTriggerExit (other : Collider) {
+
+if (other.gameObject == Player)
+
+
+animation.CrossFade("AutoDoorClose");
+}
